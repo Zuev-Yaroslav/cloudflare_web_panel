fd1789643efbff602459c820c85bc5c7d263a917
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2024-09-04 16:11:44 +0300

    98898

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -54,6 +54,12 @@ function handleImageError() {
                     </div>
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
                         <Link
+                            :href="route('cloudflare-accounts.index')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Cloudflare Accounts
+                        </Link>
+                        <Link
                             v-if="$page.props.auth.user"
                             :href="route('dashboard')"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
@@ -62,12 +68,6 @@ function handleImageError() {
                         </Link>
 
                         <template v-else>
-                            <Link
-                                :href="route('cloudflare-accounts.index')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Cloudflare Accounts
-                            </Link>
                             <Link
                                 :href="route('login')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
